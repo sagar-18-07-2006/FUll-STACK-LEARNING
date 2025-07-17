397e04a0daf664ad8ae65e53fa2f519a74e56f26
Sagar  Maheshwari <sagarmaheshwari650@gmail.com>
2025-07-17 15:35:08 +0530

    did variables js

---
+++ tutorial55/script.js
@@ -0,0 +1,26 @@
+console.log("Hey it is tutorial 55");
+var a=5;
+var b=6;
+console.log(a+b);
+console.log(a+b+8);
+console.log(typeof a,typeof b ,typeof 8);
+// const a1=6;
+// a1=a1+1;
+let x="harry";
+let y=5;
+let z=53.2;
+let sa=true;
+let q =undefined;
+let r=null;
+console.log(x,y,z,sa,q,r);
+console.log(typeof x,typeof y,typeof z,typeof sa,typeof q,typeof r);
+let o={
+    "name":"Sagar Maheswari",
+    "job code":5000
+
+}
+console.log(o)
+o.salary=10e5;
+console.log(o)
+o.salary=10e5*5;
+console.log(o)
