57f1dfc3f321fa1d6dbf6d3b3e3b4770f1b85627
Sagar  Maheshwari <sagarmaheshwari650@gmail.com>
2025-07-17 15:20:37 +0530

    did started and installed js

--- tutorial54/script.js
+++ tutorial54/script.js
@@ -1,4 +1,6 @@
-  alert("hello World ");
-  console.log("Code is running");
-  console.log("Code is running");
-  console.log("Code is running");
+alert("hello World ");
+console.log("Code is running");
+console.log("Code is running");
+console.log("Code is running");
+document.title = "Hey I am Good";
+document.body.style.backgroundColor = "red";
