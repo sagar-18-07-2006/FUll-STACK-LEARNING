2ba3b84263dd195eb478676f4ef9ff3778cd38b0
Sagar  Maheshwari <sagarmaheshwari650@gmail.com>
2025-07-18 19:03:48 +0530

    did default value parameter and return

--- tutorial58/script.js
+++ tutorial58/script.js
@@ -2,12 +2,19 @@
 // console.log("Hey Sagar You are Good");
 // console.log("Hey Sagar You are Handsome");
 // console.log("Hey Sagar You are Loyal");
-function nice(name){
-    console.log("Hey "+name+" You are nice");
-    console.log("Hey "+name+" You are Good");
-    console.log("Hey "+name+" You are Handsome");
-    console.log("Hey "+name+" You are Loyal");
-    
+function nice(name) {
+    console.log("Hey " + name + " You are nice");
+    console.log("Hey " + name + " You are Good");
+    console.log("Hey " + name + " You are Handsome");
+    console.log("Hey " + name + " You are Loyal");
+
 }
 nice("sagar");
 nice("Aman");
+function sum(a, b, c = 3) {
+    return a + b + c;
+
+}
+console.log(sum(11, 57.2))
+console.log(sum(11, .2))
+console.log(sum(25, 45,1))
