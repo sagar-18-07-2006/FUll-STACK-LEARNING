2cb47af4de91c5df912ca59b5702547072b0de3f
Sagar  Maheshwari <sagarmaheshwari650@gmail.com>
2025-07-18 22:04:54 +0530

    did that

--- tutorial60/script.js
+++ tutorial60/script.js
@@ -10,5 +10,12 @@ console.log(a[5]);
 console.log(a.length);
 let name="Sagar";
 let friend="None";
-console.log(`his name is ${name} and his friend name is ${friend}`)
 
+console.log(`his name is ${name} and his friend name is ${friend}`);
+// we can add double Quotes in this 
+console.log("Dar\"hita");// escape literal syntax  
+console.log("Dar\n  hita");// escape literal syntax  
+let b ="Harry";
+console.log(b.toUpperCase());
+console.log(b.toLowerCase());
+console.log(b.slice(1,4));//4 include nahi hai 
