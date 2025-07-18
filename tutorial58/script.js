f009bb8e77f037b28e0687a6dd5c907213036182
Sagar  Maheshwari <sagarmaheshwari650@gmail.com>
2025-07-18 18:53:30 +0530

    doing js function

---
+++ tutorial58/script.js
@@ -0,0 +1,13 @@
+// console.log("Hey Sagar You are nice");
+// console.log("Hey Sagar You are Good");
+// console.log("Hey Sagar You are Handsome");
+// console.log("Hey Sagar You are Loyal");
+function nice(name){
+    console.log("Hey "+name+" You are nice");
+    console.log("Hey "+name+" You are Good");
+    console.log("Hey "+name+" You are Handsome");
+    console.log("Hey "+name+" You are Loyal");
+    
+}
+nice("sagar");
+nice("Aman");
