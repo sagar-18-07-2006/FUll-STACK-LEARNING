c191471a12524e704e717218328ab2cf20c86e48
Sagar  Maheshwari <sagarmaheshwari650@gmail.com>
2025-07-18 20:19:18 +0530

    added this to make things easy

---
+++ tutorial60/script.js
@@ -0,0 +1,13 @@
+console.log("Thois is a Strind=gsd Tutorial")
+let a= "Sagar";
+console.log(a);
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+console.log(a.length);
+let name="Sagar";
+let friend="None";
+
