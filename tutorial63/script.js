53b875166c56a97617b6d90a52b742f406dfe44b
Sagar  Maheshwari <sagarmaheshwari650@gmail.com>
2025-07-21 21:20:25 +0530

    did the array to string

---
+++ tutorial63/script.js
@@ -0,0 +1,14 @@
+let arr =[1,2,4,5,6,7];
+// console.log(arr);
+// console.log(arr.length);
+// //arrays is mutable
+// console.log(arr[0]);  
+// arr[0]=566
+// console.log(arr[0]); 
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+// console.log(arr[4]);
+// console.log(arr[5]);
+// type of array  is object
+console.log(arr.toString);
