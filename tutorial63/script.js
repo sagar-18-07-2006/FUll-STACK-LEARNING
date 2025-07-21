345efab5e9085d77b262d6169754607f13fc4eae
Sagar  Maheshwari <sagarmaheshwari650@gmail.com>
2025-07-21 21:56:24 +0530

    did so many funcction of array

--- tutorial63/script.js
+++ tutorial63/script.js
@@ -1,27 +1,30 @@
-let arr =[1,2,4,5,6,7];
-// console.log(arr);
-// console.log(arr.length);
-// //arrays is mutable
-// console.log(arr[0]);  
-// arr[0]=566
-// console.log(arr[0]); 
-// console.log(arr[1]);
-// console.log(arr[2]);
-// console.log(arr[3]);
-// console.log(arr[4]);
-// console.log(arr[5]);
-// type of array  is object
-console.log(arr.toString);
-console.log(arr.join(" and "));
-let a = arr.pop()
-//return the last element and remove the laast element in array
-console.log(a);
-arr.push(100);
-arr.push("Harry")
+// let arr =[1,2,4,5,6,7];
+// // console.log(arr);
+// // console.log(arr.length);
+// // //arrays is mutable
+// // console.log(arr[0]);  
+// // arr[0]=566
+// // console.log(arr[0]); 
+// // console.log(arr[1]);
+// // console.log(arr[2]);
+// // console.log(arr[3]);
+// // console.log(arr[4]);
+// // console.log(arr[5]);
+// // type of array  is object
+// console.log(arr.toString);
+// console.log(arr.join(" and "));
+// let a = arr.pop()
+// //return the last element and remove the laast element in array
+// console.log(a);
+// arr.push(100);
+// arr.push("Harry")
 
-console.log(arr.shift());
-//shift beginning se nikal deta hai 
-//unshift beeggining mein add kar deta hai
-console.log(arr);
-delete arr[3];
-console.log(arr);
+// console.log(arr.shift());
+// //shift beginning se nikal deta hai 
+// //unshift beeggining mein add kar deta hai
+// console.log(arr);
+// delete arr[3];//sirf element delete kasr deta hai aur space occupied rehta hai
+
+// console.log(arr);
+let nums=[1,2,3,4,5];
+
