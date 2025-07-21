7fc5360702c970364f5603368232a2e18fcdcc07
Sagar  Maheshwari <sagarmaheshwari650@gmail.com>
2025-07-21 21:48:39 +0530

    did so many funcction of array

--- tutorial63/script.js
+++ tutorial63/script.js
@@ -12,3 +12,16 @@ let arr =[1,2,4,5,6,7];
 // console.log(arr[5]);
 // type of array  is object
 console.log(arr.toString);
+console.log(arr.join(" and "));
+let a = arr.pop()
+//return the last element and remove the laast element in array
+console.log(a);
+arr.push(100);
+arr.push("Harry")
+
+console.log(arr.shift());
+//shift beginning se nikal deta hai 
+//unshift beeggining mein add kar deta hai
+console.log(arr);
+delete arr[3];
+console.log(arr);
