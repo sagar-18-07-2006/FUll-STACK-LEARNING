d6f81424e7dac98dd198d4a2f42a138ee3348f5e
Sagar  Maheshwari <sagarmaheshwari650@gmail.com>
2025-07-21 22:45:32 +0530

    did tutorial 63

--- tutorial63/mapfilterreduce.js
+++ tutorial63/mapfilterreduce.js
@@ -1,17 +1,24 @@
-let arr=[1,3,5,7,11,13]
-// for (let index = 0; index < arr.length; index++) {
-    //   newArr.push(arr[index]**2)
-    //     }
-    //easy way 
-    let newArr=arr.map((e,index,arr)=>{
-        return e**2;
-    });
-console.log(newArr)
-const greaterThanSeven =(e)=>{
-    if(e>7){
-        return true;
-    }
-    return false;
-}
+// let arr=[1,3,5,7,11,13]
+// // for (let index = 0; index < arr.length; index++) {
+//     //   newArr.push(arr[index]**2)
+//     //     }
+//     //easy way 
+//     let newArr=arr.map((e,index,arr)=>{
+//         return e**2;
+//     });
+// console.log(newArr)
+// const greaterThanSeven =(e)=>{
+//     if(e>7){
+//         return true;
+//     }
+//     return false;
+// }
 
-console.log(newArr.filter(greaterThanSeven))
+// console.log(newArr.filter(greaterThanSeven))
+// let arr2=[1,2,3,4,5,6]
+// const red=(a,b)=>{
+//     return a+b
+// }
+// console.log(arr2.reduce(red))
+let a =Array.from("sagar")
+console.log(a);
