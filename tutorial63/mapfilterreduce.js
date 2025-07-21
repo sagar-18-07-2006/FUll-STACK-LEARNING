f025a2c344000b384d9b3c9362cfc47b32c50fc7
Sagar  Maheshwari <sagarmaheshwari650@gmail.com>
2025-07-21 22:38:23 +0530

    did this

---
+++ tutorial63/mapfilterreduce.js
@@ -0,0 +1,17 @@
+let arr=[1,3,5,7,11,13]
+// for (let index = 0; index < arr.length; index++) {
+    //   newArr.push(arr[index]**2)
+    //     }
+    //easy way 
+    let newArr=arr.map((e,index,arr)=>{
+        return e**2;
+    });
+console.log(newArr)
+const greaterThanSeven =(e)=>{
+    if(e>7){
+        return true;
+    }
+    return false;
+}
+
+console.log(newArr.filter(greaterThanSeven))
