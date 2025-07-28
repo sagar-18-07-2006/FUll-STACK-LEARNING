32882a5d7eb26a523174e8f1472d64d61a6ee057
Sagar  Maheshwari <sagarmaheshwari650@gmail.com>
2025-07-28 22:44:02 +0530

    restarting js

--- tutorial67/script.js
+++ tutorial67/script.js
@@ -1,3 +1,6 @@
 console.log(document.body);
 console.log(document.body.childNodes);
-console.log(document.body.childNodes[1]);
+console.log(document.body.childNodes[1]); 
+console.log(document.body.childNodes[1].childNodes); 
+let cont =document.body.childNodes[1];
+console.log(cont.firstChild,cont.lastChild );
