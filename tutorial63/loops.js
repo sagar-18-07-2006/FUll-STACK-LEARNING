c303174e1b9056fe4f36c1eeeb57384981631c17
Sagar  Maheshwari <sagarmaheshwari650@gmail.com>
2025-09-08 19:27:39 +0530

    did the edit

--- tutorial63/loops.js
+++ tutorial63/loops.js
@@ -1,24 +1,24 @@
 let a =[1,2,3,4,5];
-// for (let index = 0; index < a.length; index++) {
-//     const element = a[index];
-//     console.log(element);
+for (let index = 0; index < a.length; index++) {
+    const element = a[index];
+    console.log(element);
     
-// }
-// a.forEach((value,index,arr) => {
-//     console.log(value,index,arr)    
-// });
-// let obj={
-//     'a':1,
-// 'b':2,
-// 'c':3
-// };
-// for (const key in obj) {
-//     if(obj.hasOwnProperty.call(obj,key)){
-//        let element=obj[key];
-//         console.log(element);
-//     }
-// }
-// for (const element of a) {
-//     console.log(element);
+}
+a.forEach((value,index,arr) => {
+    console.log(value,index,arr)    
+});
+let obj={
+    'a':1,
+'b':2,
+'c':3
+};
+for (const key in obj) {
+    if(obj.hasOwnProperty.call(obj,key)){
+       let element=obj[key];
+        console.log(element);
+    }
+}
+for (const element of a) {
+    console.log(element);
     
-// }
+}
